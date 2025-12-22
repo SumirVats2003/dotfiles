c79cc9005e0bf68ca3383547720fc91e645b6052
Sumir Vats <65482161+SumirVats2003@users.noreply.github.com>
2025-12-22 14:27:54 +0530

    Add setup script for Arch Linux development environment
    This script sets up a development environment on Arch Linux by installing necessary packages, configuring git, and setting up zsh with various plugins and themes.

---
+++ setup.sh
@@ -0,0 +1,41 @@
+sudo pacman -Syu
+
+sudo pacman -S git zsh kitty go
+
+# installing yay
+sudo pacman -S --needed git base-devel
+git clone https://aur.archlinux.org/yay.git
+cd yay
+makepkg -si
+cd ..
+rm -rf yay
+
+yay -Y --gendb
+yay -Syu --devel
+yay -Y --devel --save
+
+# general utilities
+sudo pacman -S wofi hyprpaper waybar dunst thunar lazygit tmux shotwell fzf
+git config --global user.name "Sumir Vats"
+git config --global user.email "sumirvats@gmail.com"
+go install go.senan.xyz/cliphist@latest
+
+# shell
+chsh -s $(which zsh)
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+git clone https://github.com/mrx04programmer/ZshTheme-ArchCraft/ ~/zsh-theme
+cp ~/zsh-theme/archcraft-dwm.zsh-theme ~/.oh-my-zsh/themes/archcraft-dwm.zsh-theme
+rm -rf ~/zsh-theme
+
+git clone https://github.com/zsh-users/zsh-autosuggestions.git $ZSH_CUSTOM/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
+git clone https://github.com/zdharma-continuum/fast-syntax-highlighting.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/fast-syntax-highlighting
+git clone --depth 1 -- https://github.com/marlonrichert/zsh-autocomplete.git $ZSH_CUSTOM/plugins/zsh-autocomplete
+
+# screenshot utilty
+sudo pacman -S grim imagemagick wl-clipboard
+yay -S quickshell
+git clone https://github.com/jamdon2/hyprquickshot ~/.config/quickshell/hyprquickshot
+
+# brave
+curl -fsS https://dl.brave.com/install.sh | sh
