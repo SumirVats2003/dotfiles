2611a813ae3757f7b9d89bd43df38e0d2adf839c
Sumir Vats <65482161+SumirVats2003@users.noreply.github.com>
2025-12-22 15:25:44 +0530

    Add utilities and clone repositories in setup script

--- setup.sh
+++ setup.sh
@@ -20,18 +20,6 @@ git config --global user.name "Sumir Vats"
 git config --global user.email "sumirvats@gmail.com"
 go install go.senan.xyz/cliphist@latest
 
-# shell
-chsh -s $(which zsh)
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-git clone https://github.com/mrx04programmer/ZshTheme-ArchCraft/ ~/zsh-theme
-cp ~/zsh-theme/archcraft-dwm.zsh-theme ~/.oh-my-zsh/themes/archcraft-dwm.zsh-theme
-rm -rf ~/zsh-theme
-
-git clone https://github.com/zsh-users/zsh-autosuggestions.git $ZSH_CUSTOM/plugins/zsh-autosuggestions
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
-git clone https://github.com/zdharma-continuum/fast-syntax-highlighting.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/fast-syntax-highlighting
-git clone --depth 1 -- https://github.com/marlonrichert/zsh-autocomplete.git $ZSH_CUSTOM/plugins/zsh-autocomplete
-
 # screenshot utilty
 sudo pacman -S grim imagemagick wl-clipboard
 yay -S quickshell
@@ -39,3 +27,21 @@ git clone https://github.com/jamdon2/hyprquickshot ~/.config/quickshell/hyprquic
 
 # brave
 curl -fsS https://dl.brave.com/install.sh | sh
+
+# copy my dotfiles
+cd ~
+git clone https://github.com/SumirVats2003/dotfiles df
+cp .zshrc ~/.zshrc
+cp .tmux.conf ~/.tmux.conf
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+cp -r .config/* ~/.config/
+
+# shell
+git clone https://github.com/mrx04programmer/ZshTheme-ArchCraft/ ~/zsh-theme
+cp ~/zsh-theme/archcraft-dwm.zsh-theme ~/.oh-my-zsh/themes/archcraft-dwm.zsh-theme
+rm -rf ~/zsh-theme
+git clone https://github.com/zsh-users/zsh-autosuggestions.git $ZSH_CUSTOM/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
+git clone https://github.com/zdharma-continuum/fast-syntax-highlighting.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/fast-syntax-highlighting
+git clone --depth 1 -- https://github.com/marlonrichert/zsh-autocomplete.git $ZSH_CUSTOM/plugins/zsh-autocomplete
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
