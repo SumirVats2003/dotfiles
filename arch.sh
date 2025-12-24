9c4eac52a5bf563685cd4145bbba8127a8218400
Sumir Vats <65482161+SumirVats2003@users.noreply.github.com>
2025-12-24 20:43:46 +0530

    Update arch.sh to streamline package installations

--- arch.sh
+++ arch.sh
@@ -15,7 +15,7 @@ yay -Syu --devel
 yay -Y --devel --save
 
 # general utilities
-sudo pacman -S wofi hyprpaper waybar dunst thunar lazygit tmux shotwell fzf zathura zathura-pdf-poppler qutebrowser
+sudo pacman -S wofi hyprpaper waybar dunst thunar lazygit tmux shotwell fzf zathura zathura-pdf-poppler
 git config --global user.name "Sumir Vats"
 git config --global user.email "sumirvats@gmail.com"
 go install go.senan.xyz/cliphist@latest
@@ -25,20 +25,17 @@ sudo pacman -S grim imagemagick wl-clipboard
 yay -S quickshell
 git clone https://github.com/jamdon2/hyprquickshot ~/.config/quickshell/hyprquickshot
 
+# zen browser
+curl -fsSL https://github.com/zen-browser/updates-server/raw/refs/heads/main/install.sh | sh
+
 # copy my dotfiles
-cd ~
-git clone https://github.com/SumirVats2003/dotfiles df
+cd ~/dotfiles
 cp .zshrc ~/.zshrc
-cp .tmux.conf ~/.tmux.conf
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+cp .tmux.conf ~/.tmux.conf
 cp -r .config/* ~/.config/
 
 # shell
 git clone https://github.com/mrx04programmer/ZshTheme-ArchCraft/ ~/zsh-theme
 cp ~/zsh-theme/archcraft-dwm.zsh-theme ~/.oh-my-zsh/themes/archcraft-dwm.zsh-theme
 rm -rf ~/zsh-theme
-git clone https://github.com/zsh-users/zsh-autosuggestions.git $ZSH_CUSTOM/plugins/zsh-autosuggestions
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
-git clone https://github.com/zdharma-continuum/fast-syntax-highlighting.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/fast-syntax-highlighting
-git clone --depth 1 -- https://github.com/marlonrichert/zsh-autocomplete.git $ZSH_CUSTOM/plugins/zsh-autocomplete
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
